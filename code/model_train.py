931fa03de01864e19a99fa7829fa7d6e2e5d1fe4
icrto <up201403560@fe.up.pt>
2022-07-04 12:45:19 +0100

    changed eval method

--- code/model_train.py
+++ code/model_train.py
@@ -2,8 +2,6 @@
 import os
 import tqdm
 import numpy as np
-import argparse
-from PIL import Image
 
 # PyTorch Imports
 import torch
@@ -11,18 +9,18 @@ import torch.utils.data
 
 # Project Imports
 from data_utilities import get_transform, collate_fn, LoggiPackageDataset
-from model_utilities import LoggiBarcodeDetectionModel
-from metrics_utilities import compute_mAP_metrics, visum2022score
-
-
+from model_utilities import LoggiBarcodeDetectionModel, evaluate, visum2022score
 
 # Random seeds
 torch.manual_seed(42)
 
+from torch.utils.tensorboard import SummaryWriter
+
+
 # Constant variables
-BATCH_SIZE = 1
-NUM_EPOCHS = 1
-IMG_SIZE = 1024
+BATCH_SIZE = 4
+NUM_EPOCHS = 2
+IMG_SIZE = 512
 IOU_RANGE = np.arange(0.5, 1, 0.05)
 VAL_MAP_FREQ = 1
 
@@ -32,12 +30,15 @@ SAVE_MODEL_DIR = "results/models"
 if not os.path.isdir(SAVE_MODEL_DIR):
     os.makedirs(SAVE_MODEL_DIR)
 
-
+# Tensorboard Writer
+tb = SummaryWriter(log_dir="results/tensorboard", flush_secs=10)
 
 # Prepare data
 # First, we create two train sets with different transformations (we will use the one w/out transforms as validation set)
-dataset = LoggiPackageDataset(data_dir=DATA_DIR, training=True, transforms=get_transform(training=True, data_augment=True, img_size=IMG_SIZE))
-dataset_notransforms = LoggiPackageDataset(data_dir=DATA_DIR, training=True, transforms=get_transform(training=True, data_augment=False, img_size=IMG_SIZE))
+dataset = LoggiPackageDataset(data_dir=DATA_DIR, training=True, transforms=get_transform(
+    training=True, data_augment=True, img_size=IMG_SIZE))
+dataset_notransforms = LoggiPackageDataset(data_dir=DATA_DIR, training=True, transforms=get_transform(
+    training=True, data_augment=False, img_size=IMG_SIZE))
 
 # Split the dataset into train and validation sets
 indices = torch.randperm(len(dataset)).tolist()
@@ -48,19 +49,23 @@ val_set = torch.utils.data.Subset(dataset_notransforms, indices[-299:])
 
 # DataLoaders
 # Train loader
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, collate_fn=collate_fn)
+train_loader = torch.utils.data.DataLoader(
+    train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, collate_fn=collate_fn)
 
 # Validation loader
-val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, collate_fn=collate_fn)
+val_loader = torch.utils.data.DataLoader(
+    val_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, collate_fn=collate_fn)
 
 
 # Define DEVICE (GPU or CPU)
-DEVICE = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+DEVICE = torch.device(
+    "cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 print(f"Using device: {DEVICE}")
 
 
 # Define model
-model = LoggiBarcodeDetectionModel(min_img_size=IMG_SIZE, max_img_size=IMG_SIZE)
+model = LoggiBarcodeDetectionModel(
+    min_img_size=IMG_SIZE, max_img_size=IMG_SIZE)
 
 # Print model summary
 model_summary = model.summary()
@@ -71,7 +76,8 @@ model.to(DEVICE)
 
 # Define an optimizer
 model_params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(model_params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+optimizer = torch.optim.SGD(model_params, lr=0.005,
+                            momentum=0.9, weight_decay=0.0005)
 
 
 # Start the training and validation loops
@@ -92,21 +98,19 @@ for epoch in range(NUM_EPOCHS):
     losses_rpn_box_reg = list()
     losses_ = list()
 
-
     # Go through train loader
-    for images, targets, _ in tqdm.tqdm(train_loader):
-        
-        # Load data
-        images = list(image.to(DEVICE) for image in images)
-        targets_ = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+    for images, targets in tqdm.tqdm(train_loader):
 
+        # Load data
+        images = [image.to(DEVICE) for image in images]
+        targets_ = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
 
         # Compute loss
         loss_dict = model(images, targets_)
         losses = sum(loss for loss in loss_dict.values())
         loss_value = losses.item()
 
-        # Print loss values
+        # Save loss values
         losses_classifier.append(loss_dict['loss_classifier'].item())
         losses_box_reg.append(loss_dict['loss_box_reg'].item())
         losses_mask.append(loss_dict['loss_mask'].item())
@@ -114,142 +118,57 @@ for epoch in range(NUM_EPOCHS):
         losses_rpn_box_reg.append(loss_dict['loss_rpn_box_reg'].item())
         losses_.append(loss_value)
 
-
         # Optimise models parameters
         optimizer.zero_grad()
         losses.backward()
         optimizer.step()
-    
 
     # Print loss values
     print(f"Loss Classifier: {np.sum(losses_classifier) / len(train_set)}")
     print(f"Loss Box Regression: {np.sum(losses_box_reg) / len(train_set)}")
     print(f"Loss Mask: {np.sum(losses_mask) / len(train_set)}")
     print(f"Loss Objectness: {np.sum(losses_objectness) / len(train_set)}")
-    print(f"Loss RPN Box Regression: {np.sum(losses_rpn_box_reg) / len(train_set)}")
+    print(
+        f"Loss RPN Box Regression: {np.sum(losses_rpn_box_reg) / len(train_set)}")
     print(f"Loss: {np.sum(losses_) / len(train_set)}")
 
+    # Print loss values in tensorboard
+    tb.add_scalar('loss/clf', np.sum(
+        losses_classifier) / len(train_set), epoch)
+    tb.add_scalar('loss/boxreg', np.sum(
+        losses_box_reg) / len(train_set), epoch)
+    tb.add_scalar('loss/mask', np.sum(
+        losses_mask) / len(train_set), epoch)
+    tb.add_scalar('loss/obj', np.sum(
+        losses_objectness) / len(train_set), epoch)
+    tb.add_scalar('loss/rpn', np.sum(
+        losses_rpn_box_reg) / len(train_set), epoch)
+    tb.add_scalar('loss/total_loss', np.sum(
+        losses_) / len(train_set), epoch)
 
-    
-
-    if (epoch % VAL_MAP_FREQ == 0 and epoch > 0) or (epoch == NUM_EPOCHS - 1):
+    if ((epoch + 1) % VAL_MAP_FREQ == 0) or (epoch == NUM_EPOCHS - 1):
         # Validation Phase
-        print("Validation Phase")
-        model.eval()
+        eval_results = evaluate(model, val_loader, DEVICE)
+        bbox_results = eval_results.coco_eval['bbox']
+        segm_results = eval_results.coco_eval['segm']
+        bbox_map = bbox_results.stats[0]
+        segm_map = segm_results.stats[0]
+        visum_score = visum2022score(bbox_map, segm_map)
 
-        # Create dictionaries for ground-truth and predictions
-        groundtruth_data = dict()
-        predictions_data = dict()
+        # Print mAP values
+        print(f"Detection mAP: {np.round(bbox_map, 4)}")
+        print(f"Segmentation mAP: {np.round(segm_map, 4)}")
+        print(f"VISUM Score: {np.round(visum_score, 4)}")
 
-        with torch.no_grad():
-            
-            # Go through validation loader
-            for images, targets, image_fnames in tqdm.tqdm(val_loader):
-
-                # Load data
-                images = list(img.to(DEVICE) for img in images)
-                targets_ = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
-                image_fnames_ = [f for f in image_fnames]
-                outputs = model(images, targets)
-
-
-                # Add to ground truth list
-                for out, t, fname in zip(outputs, targets_, image_fnames_):
-
-                    # Create dictionaries for groundtruth data
-                    groundtruth_data[fname] = dict()
-                    groundtruth_data[fname]['boxes'] = list()
-                    groundtruth_data[fname]['scores'] = list()
-                    groundtruth_data[fname]['masks'] = list()
-
-                    # i = 0
-                    for bb, mask in zip(t["boxes"], t["masks"]):
-                        # Bounding-boxes
-                        groundtruth_data[fname]['boxes'].append(list(bb.detach().cpu().numpy()))
-                        
-                        # Masks
-                        # msk_fname = f"{i}.jpg"
-                        # groundtruth_data[fname]['masks'].append(msk_fname)
-                        # print(f'Masks shape: {t["masks"].shape}')
-
-                        # Save masks into directory
-                        msk_ = mask.detach().cpu().numpy().copy()
-                        groundtruth_data[fname]['masks'].append(msk_)
-                        
-                        # pil_mask = Image.fromarray(msk_).convert("L")
-                        
-                        # Save into temporary directory
-                        # if not os.path.isdir(os.path.join("results", "validation", "masks", "gt", fname.split('.')[0])):
-                            # os.makedirs(os.path.join("results", "validation", "masks", "gt", fname.split('.')[0]))
-                
-                        # pil_mask.save(os.path.join("results", "validation", "masks",  "gt", fname.split('.')[0], msk_fname))
-
-                        # Update i (idx)
-                        # i += 1
-
-
-                    # Create dictionaries for predictions data
-                    predictions_data[fname] = dict()
-                    predictions_data[fname]['boxes'] = list()
-                    predictions_data[fname]['scores'] = list()
-                    predictions_data[fname]['masks'] = list()
-
-                    # j = 0
-                    for bb, mask, score in zip(out["boxes"], out["masks"], out["scores"]):
-                        # Bounding-boxes
-                        predictions_data[fname]['boxes'].append(list(bb.detach().cpu().numpy()))
-
-                        # Scores
-                        predictions_data[fname]['scores'].append(float(score.detach().cpu()))
-
-                        # Masks
-                        # msk_fname = f"{i}.jpg"
-                        # predictions_data[fname]['masks'].append(msk_fname)
-
-                        # Save masks into directory
-                        msk_ = np.squeeze(a=mask.detach().cpu().numpy().copy(), axis=0)
-                        predictions_data[fname]['masks'].append(msk_)
-                        # pil_mask = Image.fromarray(msk_).convert("L")
-                        
-                        # Save into temporary directory
-                        # if not os.path.isdir(os.path.join("results", "validation", "masks", "pred", fname.split('.')[0])):
-                            # os.makedirs(os.path.join("results", "validation", "masks", "pred", fname.split('.')[0]))
-                
-                        # pil_mask.save(os.path.join("results", "validation", "masks",  "pred", fname.split('.')[0], msk_fname))
-
-                        # Update j (idx)
-                        # j += 1
-        
-
-        # TODO: Erase uppon review
-        # Compute validation metrics
-        # predictions_dir = os.path.join("results", "validation", "masks",  "pred")
-        # groundtruth_dir = os.path.join("results", "validation", "masks",  "gt")
-
-        # bboxes_mAP, bboxes_APs, masks_mAP, masks_APs = compute_mAP_metrics(predictions_data, groundtruth_data, predictions_dir, groundtruth_dir)
-        bboxes_mAP, bboxes_APs, masks_mAP, masks_APs = compute_mAP_metrics(predictions_data, groundtruth_data)
-        
-
-        # Bounding-boxes mAP
-        print("Bounding-boxes mAP:{:.3f}".format(bboxes_mAP))
-        for ap_metric, iou in zip(bboxes_APs, IOU_RANGE):
-            print("\tBounding-boxes AP at IoU level [{:.2f}]: {:.3f}".format(iou, ap_metric))
-        
-        # Masks mAP
-        print("Masks mAP:{:.3f}".format(masks_mAP))
-        for ap_metric, iou in zip(masks_APs, IOU_RANGE):
-            print("\tMasks AP at IoU level [{:.2f}]: {:.3f}".format(iou, ap_metric))
-
-
-        # Compute VISUM SCORE 2022
-        score = visum2022score(bboxes_mAP=bboxes_mAP, masks_mAP=masks_APs)
-        print(f"VISUM2020 Score on Validation: {score}")
-
-
+        # Print mAP values in tensorboard
+        tb.add_scalar('eval/bbox_map', bbox_map, epoch)
+        tb.add_scalar('eval/segm_map', segm_map, epoch)
+        tb.add_scalar('eval/visum_score', visum_score, epoch)
 
     torch.save(model.state_dict(), os.path.join(SAVE_MODEL_DIR, "visum2022.pt"))
 
-    print(f"Model successfully saved at {os.path.join(SAVE_MODEL_DIR, 'visum2022.pt')}")
+    print(
+        f"Model successfully saved at {os.path.join(SAVE_MODEL_DIR, 'visum2022.pt')}")
 
 
 print("Finished.")
--- code/model_utilities.py
+++ code/model_utilities.py
@@ -6,10 +6,12 @@ import torchvision
 from torchvision.models.detection import MaskRCNN
 from torchvision.models.detection.anchor_utils import AnchorGenerator
 from torchinfo import summary
-
-
+from tqdm import tqdm
+from coco_eval import CocoEvaluator, convert_to_coco_api
 
 # Model: LoggiBarcodeDetectionModel
+
+
 class LoggiBarcodeDetectionModel(torch.nn.Module):
     def __init__(self, min_img_size=800, max_img_size=1333, nr_classes=2, backbone="mobilenet_v2", backbone_pretrained=True):
         super(LoggiBarcodeDetectionModel, self).__init__()
@@ -23,50 +25,96 @@ class LoggiBarcodeDetectionModel(torch.nn.Module):
         if self.backbone == "mobilenet_v2":
             # Source: https://pytorch.org/vision/stable/_modules/torchvision/models/detection/mask_rcnn.html#maskrcnn_resnet50_fpn
             # Load a pre-trained model for classification and return only the features
-            backbone_ = torchvision.models.mobilenet_v2(pretrained=self.backbone_pretrained).features
+            backbone_ = torchvision.models.mobilenet_v2(
+                pretrained=self.backbone_pretrained).features
             # MaskRCNN needs to know the number of output channels in a backbone
             # For mobilenet_v2, it's 1280, so we need to add it here
             backbone_.out_channels = 1280
 
             # Let's make the RPN generate 5 x 3 anchors per spatial location, with 5 different sizes and 3 different aspect ratios
             # We have a Tuple[Tuple[int]] because each feature map could potentially have different sizes and aspect ratios
-            anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
+            anchor_generator = AnchorGenerator(
+                sizes=((32, 64, 128, 256, 512),), aspect_ratios=((0.5, 1.0, 2.0),))
 
             # Let's define what are the feature maps that we will use to perform the region of interest cropping, as well as the size of the crop after rescaling
             # If your backbone returns a Tensor, featmap_names is expected to be ['0']
             # More generally, the backbone should return an OrderedDict[Tensor], and in featmap_names you can choose which feature maps to use
-            roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=7, sampling_ratio=2)
-            mask_roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], output_size=14, sampling_ratio=2)
+            roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+                featmap_names=['0'], output_size=7, sampling_ratio=2)
+            mask_roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+                featmap_names=['0'], output_size=14, sampling_ratio=2)
 
             # Put the pieces together inside a MaskRCNN model
-            self.model = MaskRCNN(backbone_, min_size=min_img_size, max_size=max_img_size, num_classes=2, rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler, mask_roi_pool=mask_roi_pooler)
-
+            self.model = MaskRCNN(backbone_, min_size=min_img_size, max_size=max_img_size, num_classes=2,
+                                  rpn_anchor_generator=anchor_generator, box_roi_pool=roi_pooler, mask_roi_pool=mask_roi_pooler)
 
         # You can add your backbones here...
         # elif self.backbone == "your_backbone_name"
 
-
         return
-    
 
-    def forward(self, inputs, targets):
+    def forward(self, inputs, targets=None):
 
         # Compute outputs
-        if self.training == True:
+        if targets and self.training:
             outputs = self.model(inputs, targets)
-        
+
         else:
             outputs = self.model(inputs)
 
-
         return outputs
-    
-    
+
     def summary(self):
-        
+
         return summary(self.model, (1, 3, 1024, 1024))
 
 
+@torch.inference_mode()
+def evaluate(model, data_loader, device):
+    print("Evaluating...")
+    n_threads = torch.get_num_threads()
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    model.eval()
+
+    # coco = get_coco_api_from_dataset(data_loader.dataset)
+    coco = convert_to_coco_api(data_loader.dataset)
+    iou_types = ['bbox', 'segm']
+    coco_evaluator = CocoEvaluator(coco, iou_types)
+
+    for images, targets in tqdm(data_loader):
+        images = list(img.to(device) for img in images)
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        outputs = model(images)
+
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()}
+                   for t in outputs]
+
+        res = {target["image_id"].item(): output for target,
+               output in zip(targets, outputs)}
+        coco_evaluator.update(res)
+
+    coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    coco_evaluator.accumulate()
+    coco_evaluator.summarize()
+    torch.set_num_threads(n_threads)
+    return coco_evaluator
+
+
+# Function: Compute VISUM 2022 Competition Metric
+def visum2022score(bboxes_mAP, masks_mAP, bboxes_mAP_weight=0.5):
+
+    # Compute masks_mAP_weight from bboxes_mAP_weight
+    masks_mAP_weight = 1 - bboxes_mAP_weight
+
+    # Compute score, i.e., score = 0.5*bboxes_mAP + 0.5*masks_mAP
+    score = (bboxes_mAP_weight * bboxes_mAP) + (masks_mAP_weight * masks_mAP)
+
+    return score
 
 
 # Run this file to test the LoggiBarcodeDetectionModel class
